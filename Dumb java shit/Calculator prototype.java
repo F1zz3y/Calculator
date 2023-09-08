a885ee55a4bebb6109ba48061c0a38fe3efa49f9
F1zz3y_ <65451753+F1zz3y@users.noreply.github.com>
2023-09-08 19:37:42 -0400

    Calculator prototype.java

--- Dumb java shit/Calculator prototype.java
+++ Dumb java shit/Calculator prototype.java
@@ -1,4 +1,4 @@
-import java.time.LocalDateTime;
+import java.time.LocalDateTime; //Tools needed for functions later
 import java.time.format.DateTimeFormatter;
 
 public class Main {
@@ -6,16 +6,16 @@ public class Main {
     LocalDateTime myDateTime = LocalDateTime.now();
     DateTimeFormatter myFormatTime = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");
     String formattedDate = myDateTime.format(myFormatTime);
-    System.out.println("The time is: " + formattedDate);
+    System.out.println("The time is: " + formattedDate); //sets the date and time to tell the user
     System.out.println(" ");
     int X = 420;
-    int Y = 69;
+    int Y = 69; //Based on what user wants change the variable
     int Z = 0;
-    if (X == 0) {
+    if (X == 0) { //missed variable ^
       System.out.println("2 Number addition is");
       System.out.println(Y + Z);
       System.out.println(" ");
-      System.out.println("2 Number multiplication is");
+      System.out.println("2 Number multiplication is"); //Does the calculations with the used variables
       System.out.println(Y * Z);
       System.out.println(" ");
       System.out.println("2 Number division is");
@@ -28,7 +28,7 @@ public class Main {
       System.out.println("2 Number addition is");
       System.out.println(X + Z);
       System.out.println(" ");
-      System.out.println("2 Number multiplication is");
+      System.out.println("2 Number multiplication is"); //same thing
       System.out.println(X * Z);
       System.out.println(" ");
       System.out.println("2 Number division is");
@@ -41,7 +41,7 @@ public class Main {
       System.out.println("2 Number addition is");
       System.out.println(X + Y);
       System.out.println(" ");
-      System.out.println("2 Number multiplication is");
+      System.out.println("2 Number multiplication is"); //same thing
       System.out.println(X * Y);
       System.out.println(" ");
       System.out.println("2 Number division is");
@@ -52,11 +52,11 @@ public class Main {
       }
     if (X != 0) {
       if (Y != 0) {
-        if (Z != 0) {
+        if (Z != 0) { //if all variables have a value
           System.out.println("3 Number addition is");
           System.out.println(X + Y + Z);
           System.out.println(" ");
-          System.out.println("3 Number multiplication is");
+          System.out.println("3 Number multiplication is"); //calculates with 3 variables
           System.out.println(X * Y * Z);
           System.out.println(" ");
           System.out.println("3 Number division is");
@@ -68,4 +68,4 @@ public class Main {
       }
     }
   }
-}
+} //closes the code blocks to end the system
