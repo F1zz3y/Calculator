f869c2e088a57e4122112805f76c7efb14dc9917
F1zz3y_ <65451753+F1zz3y@users.noreply.github.com>
2023-09-09 00:10:17 -0400

    Update Calculator.java

--- Calculator.java
+++ Calculator.java
@@ -3,6 +3,9 @@ import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
 public class Calculator {
+  /**
+   * @param args
+   */
   public static void main(String args[]) {
     LocalDateTime myDateTime = LocalDateTime.now();
     DateTimeFormatter myFormatTime = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");
